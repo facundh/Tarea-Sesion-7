d8d94a3ad65776a352a85345c33cda3465f1820d
Facundo Herrera <facundhfed@gmail.com>
2022-07-12 10:07:27 +0200

    Sesion 7 finalizada

---
+++ tarea_sesion_7.js
@@ -0,0 +1,7 @@
+const setFamilia = new Set(['Facundo', 'Ligia', 'Bruna', 'Atila']);
+console.log(setFamilia);
+
+setFamilia.add('Facundo');
+console.log(setFamilia);
+setFamilia.add('Javascript');
+console.log(setFamilia);
